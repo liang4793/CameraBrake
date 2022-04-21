c2c03b0cf5b9663e1c2bdd2883a5aa3ac4a08a26
liang4793 <3500506060@qq.com>
2022-04-21 17:33:39 +0800

    add toaster switch

--- Emergency-Brakes.py
+++ Emergency-Brakes.py
@@ -75,7 +75,7 @@ def main():
 
     '''tk window'''
     def window_thread():
-        def rewrite_log(value):
+        def rewrite_log_loger(value):
             def changeline(line,content):
                 log = open('log.txt','r+')
                 flist = log.readlines()
@@ -89,6 +89,20 @@ def main():
             else:
                 changeline(1, "False")
                 #print("Logwriter:False")
+        def rewrite_log_toaster(value):
+            def changeline(line,content):
+                log = open('log.txt','r+')
+                flist = log.readlines()
+                flist[int(line)] = str(content) + "\n"
+                log = open('log.txt','w+')
+                log.writelines(flist)
+                log.close()
+            if value == "1":
+                changeline(2, "True")
+                #print("Toaster:True")
+            else:
+                changeline(2, "False")
+                #print("Toaster:False")
         window = tk.Tk()
         notebook = tk.ttk.Notebook(window)
         window.title("Emergency-Brakes")
@@ -99,16 +113,33 @@ def main():
         notebook.add(main_frame, text='main')
         notebook.add(setting_frame, text='setting')
         notebook.pack(padx=0, pady=0, fill=tkinter.BOTH, expand=True)
-        log_switch_label = tk.Label(setting_frame, text="Logger (left:Off; Right:On)")
-        log_switch_label.pack(anchor='nw')
-        log_switch = tk.Scale(setting_frame, from_=0, to=1,
+        #setting_page
+        loger_switch_label = tk.Label(setting_frame, text="Logger (left:Off; Right:On)")
+        loger_switch_label.pack(anchor='nw')
+        loger_switch = tk.Scale(setting_frame, from_=0, to=1,
                                orient='horizontal', length=50, width=20,
                                showvalue=0,
-                               command=rewrite_log)
-        log_switch.pack(anchor='nw')
-        
+                               command=rewrite_log_loger)
+        loger_switch.pack(anchor='nw')
+
+        toaster_switch_label = tk.Label(setting_frame, text="Toaster (left:Off; Right:On)")
+        toaster_switch_label.pack(anchor='nw')
+        toaster_switch = tk.Scale(setting_frame, from_=0, to=1,
+                               orient='horizontal', length=50, width=20,
+                               showvalue=0,
+                               command=rewrite_log_toaster)
+        toaster_switch.pack(anchor='nw')
+        #main_page
+        cam_state_show = tk.StringVar()
+        cam_state_show.set("Camera: Off")
+        while True:
+            if status_service("FrameServer") == True:
+                cam_state_show.set("Camera: On")
+            else:
+                cam_state_show.set("Camera: Off")
+            #tk.Label(main_frame, textvariable=cam_state_show).pack(anchor='nw')
+            window.update()    
         # Todo: finish the window, minimize to tray
-        window.mainloop()
 
     '''detect'''
     def detect_thread():
@@ -117,16 +148,19 @@ def main():
             log = open("log.txt", "r+")
             line = log.read().splitlines()
             list1 = line[1:2]
-            logger = ''.join(list1)
+            list2 = line[2:3]
+            logger_is_on = ''.join(list1)
+            toaster_is_on = ''.join(list2)
             log.close()
             # Todo: Modification notification mechanism
             '''FrameServer => Camera'''
             if status_service("FrameServer") == True:
-                toaster.show_toast("E-B:Camera detected being used!",
-                                   "If you aren't using it, open Emergency-Brakes and click 'Brake'.",
-                                   icon_path="image/warning.ico",
-                                   threaded=False)
-                if logger == "True":
+                if toaster_is_on == "True":
+                    toaster.show_toast("E-B:Camera detected being used!",
+                                       "If you aren't using it, open Emergency-Brakes and click 'Brake'.",
+                                       icon_path="image/warning.ico",
+                                       threaded=False)
+                if logger_is_on == "True":
                     writelog("Camera")
 
     '''thread'''
